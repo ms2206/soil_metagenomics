57df7d5e6fad5849163dd19e92b2d06fb5c99b7a
Matthew Spriggs <matspriggs@gmail.com>
2025-01-24 11:56:01 +0000

    added lunix sed command

--- scripts/S11_R_launcher.sh
+++ scripts/S11_R_launcher.sh
@@ -45,10 +45,13 @@ base_folder="/mnt/beegfs/home/s430452/soil_metagenomics"
 results_folder="${base_folder}/results"
 samplesheet="${base_folder}/data/tabsamplesheet.txt"
 
+
 # tabsamplesheet header has a '#' charecter that is causing
 # parse issues into R using read.table().
 # remove in place the '#'
-sed -i '' '1s/#//' "${samplesheet}"
+
+# sed -i '' '1s/#//' "${samplesheet}" <-- MacOS
+sed -i '1s/#//' "$samplesheet" # <-- Lunix
 
 # Launch R script
 Rscript s11_q2_to_R_assignment.R 
